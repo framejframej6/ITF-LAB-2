28aa619a56cc1cf547b976cc647912fe3a803d02
framejframej6 <71111193+framejframej6@users.noreply.github.com>
2020-10-30 10:39:08 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Hello world</title>
+    </head>
+    <body>
+        <h1>My First Heading</h1>
+        <p>นาย พสุธา จันทร์มณี เฟรม</p>
+        <img src="cat.jpg"
+        <br>
+        <img src="arcwarden.jpg"
+        <br>
+        <a href="https://www.dotabuff.com/">this is a Link</a>
+        <h2>เรื่องตลกที่ผมก็อปมาจากเน็ต</h2>
+        <ul>
+            <li>ครู : นร.จะเลือกอะไร ระหว่างเงิน กับสมอง</li>
+            <li>นร. : หนูเลือกเงินค่ะ</li>
+            <li>ครู : แต่ถ้าเป็นครู ครูจะเลือกสมองนะ</li>
+            <li>ครู : ใช่ค่ะ….ทุกคนก็ต้องเลือกสิ่งที่ตัวเองไม่มี</li>
+        </ul>
+    </body>
+</html>
